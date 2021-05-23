09fa85af1a17696551030cc1153e46ff2d7fd9e1
Daniel Amoateng <danny@Daniels-MacBook-Pro.local>
2021-05-23 15:23:32 -0400

    Set up express server and listening on port 3000

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+
+})
+
+
+
+
+
+const port  = 3000
+app.listen(port, () => {
+    console.log(`listening om port ${port}`)
+})
+
+
