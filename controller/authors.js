b730135d1ddf3d4497948e3c5ea079cdc5003048
Daniel Amoateng <danny@Daniels-MacBook-Pro.local>
2021-05-23 22:14:41 -0400

    Set up genre controller

--- controller/authors.js
+++ controller/authors.js
@@ -9,8 +9,7 @@ const getAllAuthors = async (req, res) => {
 const getAuthorById = async (request, response) => { const { id } = request.params
   const authorById = await models.Authors.findOne({ where: { id }, include: [{ model: models.Authors }] })
 
-  return authorById ? response.send(authorById) : response.sendStatus(404) 
-
+  return authorById ? response.send(authorById) : response.sendStatus(404)
 }
 
 
