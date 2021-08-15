6de77a332f3ef5463c3419bc6d6277efd92f22c2
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-08-15 18:15:53 -0400

    Create a findOne funtion for authors

--- controller/authors.js
+++ controller/authors.js
@@ -8,13 +8,33 @@ const getAllAuthors = async (req, res) => {
 
 
 
-const getAuthorById = async (request, response) => { const { id } = request.params
-  const authorById = await models.Authors.findOne({ where: { id }, include: [{ model: models.Authors }] })
+// const getAuthorById = async (request, response) => { const { id } = request.params
+//   const authorById = await models.Authors.findOne({ where: { id }, include: [{ model: models.Authors }] })
 
-  return authorById ? response.send(authorById) : response.sendStatus(404)
+//   return authorById ? response.send(authorById) : response.sendStatus(404)
+// }
+
+const getAuthorById = async (req, res) => {
+const { id } = req.params
+
+
+const author = await models.Authors.findOne({
+    where: {
+      [models.Sequelize.Op.or]: [
+        { id: id },
+        { nameLast: { [models.Sequelize.Op.like]: `%${identifier}%` } },
+      ]
+    },
+    include: [{
+      model: models.Novels,
+      include: [{ model: models.Genres }]
+    }]
+  })
+
+  return author
+    ? res.send(author)
+    : res.sendStatus(404)
 }
-
-
 
 
 module.exports = {
